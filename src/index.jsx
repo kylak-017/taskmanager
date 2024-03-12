c013ec40db638050e8843140d042db6815763878
JinGee <jingee.kim9@gmail.com>
2024-03-12 22:41:38 +0900

    Initial Upload

---
+++ src/index.jsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./routes/Root";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
